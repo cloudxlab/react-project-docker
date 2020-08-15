298734612b5988ac22b92612e2017b9211a2ab13
Abhinav Singh <abhinav.foss@gmail.com>
2020-08-15 15:43:36 +0000

    petty changes in App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload by Abhinav.
+          Edit <code>src/App.js</code> and save to reload by Abhinav 124.
         </p>
         <a
           className="App-link"
