94f4bd0dc7c34cf7530d0b83ea696c5c19202afa
Subhashree Mohapatra <lipi.devops@gmail.com>
2020-08-16 16:19:35 +0000

    APP.js file changed

--- src/App.js
+++ src/App.js
@@ -8,12 +8,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload by Subhashree 111111111111111111111111111111111111111111111111111111111111111     className="App-link"
+          Edit <code>src/App.js</code> and save to reload by Subhashree 111     
+        </p>
+        <a
+          className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+        Learn React
         </a>
       </header>
     </div>
