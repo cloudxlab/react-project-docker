2b7f1657480aec6b97218c85b83ad35e05f83586
khushkk7 <khushks@gmail.com>
2020-08-16 16:00:44 +0000

    modified src/App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload by khush.
+          Edit <code>src/App.js</code> and save to reload by khushk.
         </p>
         <a
           className="App-link"
