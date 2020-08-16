0c43c8622289cf392090fd0d477e07f087cb5707
Cloud User <Ravi@govindu.com>
2020-08-16 16:02:25 +0000

    pushed new hellow orld

--- src/App.js
+++ src/App.js
@@ -6,9 +6,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload by Ravi Govindu.
+        <img src={logo} className="App-logo" alt="logo" />        <p>
+          Edit <code>src/App.js</code> and save to reload HELLO WORLD  by Ravi Govindu.
         </p>
         <a
           className="App-link"
