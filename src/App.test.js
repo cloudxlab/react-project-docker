acd3bf332c1eea49ebe31428de2995917f13ade1
Cloud User <Ravi@govindu.com>
2020-08-16 03:56:56 +0000

    testcase line added modified

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
-'#test line ravi - testcase added
+
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/learn react/i);
