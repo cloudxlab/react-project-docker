951cd1ddd9afd11bd74607c717286d41bc82a1c1
Cloud User <centos@ip-172-31-41-87.us-east-2.compute.internal>
2020-08-18 11:44:13 +0000

    Petty changes in App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload by Abhinav.
+          Edit <code>src/App.js</code> and save to reload by Nilesh.
         </p>
         <a
           className="App-link"
