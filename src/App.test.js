49cd1627a151c7b43e519d765e016c1b1369f7b5
Cloud User <Ravi@govindu.com>
2020-08-22 15:05:19 +0000

    test Ravi

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learpppppn react/i);
+  const linkElement = getByText(/Ravi/i);
   expect(linkElement).toBeInTheDocument();
 });
