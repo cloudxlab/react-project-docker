c46a5058112308336f2ad81786cb79bc3706c3f1
khushkk7 <khushks@gmail.com>
2020-08-22 15:24:25 +0000

    modified src/App.js for key pair test

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload by khushk(feature_a).
+          Edit <code>src/App.js</code> and save to reload by khushk(feature_a)(keypair_test).
         </p>
         <a
           className="App-link"
