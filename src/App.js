698b75e0804fb645ee49f4a084abee8d90adcaaf
Subhashree Mohapatra <lipi.devops@gmail.com>
2020-08-22 16:42:24 +0000

    App file chnaged

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload by Aarush Pattnaik Web Site     
         </p>
         <p>
-        Added one more line
+        Added one more line in master brach
         </p>
         <a
           className="App-link"
