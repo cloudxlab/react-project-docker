b06210a11988e67d436a52fda27693839b528727
Cloud User <Ravi@govindu.com>
2020-08-29 12:52:51 +0000

    updated 29 AUg

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />        <p>
-          Edit <code>src/App.js</code> and save to ****AWS Travic CI ****** HELLO  by Ravi Govindu. - updated 29 Aug 2020
+          Edit <code>src/App.js</code> and save to ****AWS Travic CI ****** HELLO  by Ravi Govindu. - updated 29 Aug 202qw0
         </p>
         <a
           className="App-link"
