2e97aa49d00a9dc9ed67aa71c77c88be54b1b9b3
Cloud User <Ravi@govindu.com>
2020-08-29 13:27:12 +0000

    updated signupfeature_b->staging

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />        <p>
-          Edit <code>src/App.js</code> and save to ****AWS Travic CI ****** HELLO  by Ravi Govindu. - updated 29 Aug 202qw0
+          Edit <code>src/App.js</code> and save to signupfeature_b -> staging(already existing) - > deployment
         </p>
         <a
           className="App-link"
