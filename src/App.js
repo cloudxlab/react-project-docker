d90356c467218dfc9d4c943831f57ee339f8392e
Cloud User <Ravi@govindu.com>
2020-08-29 13:14:37 +0000

    updated with signup branch feature

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React on sign up branch
         </a>
       </header>
     </div>
